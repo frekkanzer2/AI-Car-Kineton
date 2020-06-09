e69a12e7bdbcc746799fd550b5737cc05b032e92
frekkanzer2 <abatefrancesco98@gmail.com>
2020-06-09 16:07:22 +0200

    Better acceleration of the car
    Co-Authored-By: Carmine Ferrara <cferrara98@users.noreply.github.com>

--- AI Car Kineton/Assets/Scripts/CarBehaviour.cs
+++ AI Car Kineton/Assets/Scripts/CarBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CarBehaviour : MonoBehaviour {
 
     public float staticSpeed = 5000f;
+    public float brakeForce = 100000f;
+    private float decrement = 0.05f;
     private Rigidbody carBody;
     private WheelCollider wc_fl, wc_fr, wc_bl, wc_br; // f -> front, l -> left, r -> right
 
@@ -24,11 +26,30 @@ public class CarBehaviour : MonoBehaviour {
         wc_bl.motorTorque = v;
         wc_br.motorTorque = v;
         if (Input.GetKey(KeyCode.Q)) {
-            wc_bl.brakeTorque = staticSpeed * 100;
-            wc_br.brakeTorque = staticSpeed * 100;
+            wc_bl.brakeTorque = brakeForce;
+            wc_br.brakeTorque = brakeForce;
+            wc_fl.brakeTorque = brakeForce;
+            wc_fr.brakeTorque = brakeForce;
+            if ((carBody.velocity.x > 1 || carBody.velocity.x < -1) || (carBody.velocity.y > 1 || carBody.velocity.y < -1) || (carBody.velocity.z > 1 || carBody.velocity.z < -1)) {
+                Debug.Log("Breaking");
+                if (carBody.velocity.x > 1)
+                    carBody.velocity = new Vector3(carBody.velocity.x - decrement, carBody.velocity.y, carBody.velocity.z);
+                if (carBody.velocity.x < -1)
+                    carBody.velocity = new Vector3(carBody.velocity.x + decrement, carBody.velocity.y, carBody.velocity.z);
+                if (carBody.velocity.y > 1)
+                    carBody.velocity = new Vector3(carBody.velocity.x, carBody.velocity.y - decrement, carBody.velocity.z);
+                if (carBody.velocity.y < -1)
+                    carBody.velocity = new Vector3(carBody.velocity.x, carBody.velocity.y + decrement, carBody.velocity.z);
+                if (carBody.velocity.z > 1)
+                    carBody.velocity = new Vector3(carBody.velocity.x, carBody.velocity.y, carBody.velocity.z - decrement);
+                if (carBody.velocity.z < -1)
+                    carBody.velocity = new Vector3(carBody.velocity.x, carBody.velocity.y, carBody.velocity.z + decrement);
+            }
         } else {
             wc_bl.brakeTorque = 0;
             wc_br.brakeTorque = 0;
+            wc_fl.brakeTorque = 0;
+            wc_fr.brakeTorque = 0;
         }
 
     }
