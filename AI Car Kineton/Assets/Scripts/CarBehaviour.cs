d4c76910706e537297fcb9c6fff2f92edc27408a
frekkanzer2 <abatefrancesco98@gmail.com>
2020-06-10 12:11:35 +0200

    Perfect car movement
    Better brake and acceleration

--- AI Car Kineton/Assets/Scripts/CarBehaviour.cs
+++ AI Car Kineton/Assets/Scripts/CarBehaviour.cs
@@ -6,14 +6,18 @@ public class CarBehaviour : MonoBehaviour {
 
     public float staticSpeed = 5000f;
     public float brakeForce = 100000f;
+    private float speed = 0;
     private Rigidbody carBody;
     private WheelCollider wc_fl, wc_fr, wc_bl, wc_br; // f -> front, l -> left, r -> right
     private GameObject wheel_front_left, wheel_front_right, wheel_back_left, wheel_back_right;
     private GameObject steering_wheel;
+    private Direction lastDirection, actualDirection;
+    private bool isBraking = false;
     Vector3 myPosition; Quaternion myRotation;
 
     // Start is called before the first frame update
     void Start() {
+        lastDirection = Direction.Stop;
         myPosition = new Vector3(0, 0, 0);
         myRotation = new Quaternion(0, 0, 0, 0);
         carBody = GetComponent<Rigidbody>();
@@ -39,25 +43,74 @@ public class CarBehaviour : MonoBehaviour {
 
     }
 
+    private enum Direction {
+        Acceleration,
+        Backward,
+        Stop
+    }
+
     private void verticalMovement() {
+
+        /*
+            Car uses speed to move.
+            The v parameter is an acceleration gain on the speed.
+         */
+
+        // Getting acceleration
         float v = Input.GetAxis("Vertical") * staticSpeed;
-        Debug.Log(v);
-        wc_bl.motorTorque = v;
-        wc_br.motorTorque = v;
-        if (Input.GetKey(KeyCode.Q)) {
-            wc_bl.brakeTorque = brakeForce;
-            wc_br.brakeTorque = brakeForce;
-            wc_fl.brakeTorque = brakeForce;
-            wc_fr.brakeTorque = brakeForce;
-            decrementSpeed(1f, 0.035f);
+        if (v > 1000) v = 1000;
+        if (v < -1000) v = -1000;
+        speed += v;
+        // Checking acceleration limit
+        if (speed > staticSpeed) speed = staticSpeed;
+        if (speed < staticSpeed * (-1)) speed = staticSpeed * (-1);
+        // Getting direction
+        float temp = Input.GetAxisRaw("Vertical");
+        switch (temp) {
+            case 1:
+                actualDirection = Direction.Acceleration;
+                break;
+            case -1:
+                actualDirection = Direction.Backward;
+                break;
+            case 0:
+                actualDirection = Direction.Stop;
+                break;
+        }
+        // Getting localVelocity
+        Vector3 localVelocity = transform.InverseTransformDirection(carBody.velocity);
+        if (localVelocity.z > 0.1f && actualDirection == Direction.Backward) {
+            Debug.Log(localVelocity);
+            Debug.Log("QUI");
+            brake(0.035f);
+            isBraking = true;
+            speed = 0;
+        } else if (localVelocity.z < 0.1f * (-1) && actualDirection == Direction.Acceleration) {
+            Debug.Log("QUA");
+            brake(0.035f * 2);
+            isBraking = true;
+            speed = 0;
         } else {
+            isBraking = false;
+        }
+        // Manual brake
+        if (Input.GetKey(KeyCode.Q)) {
+            brake(0.035f);
+        } else if (!isBraking) {
+            Debug.Log("Not braking");
             wc_bl.brakeTorque = 0;
             wc_br.brakeTorque = 0;
             wc_fl.brakeTorque = 0;
             wc_fr.brakeTorque = 0;
+            wc_bl.motorTorque = speed;
+            wc_br.motorTorque = speed;
         }
-        if (v == 0) {
-            decrementSpeed(0.06f, 0.05f);
+        // Idle
+        if (actualDirection == Direction.Stop) brake(0.005f);
+        if (localVelocity == Vector3.zero) {
+            speed = 0;
+            v = 0;
+            actualDirection = Direction.Stop;
         }
     }
 
@@ -65,8 +118,6 @@ public class CarBehaviour : MonoBehaviour {
         float h = Input.GetAxis("Horizontal") * 35;
         wc_fl.steerAngle = h;
         wc_fr.steerAngle = h;
-        /*wheel_front_left.transform.localRotation = Quaternion.Euler(0, h, 0);
-        wheel_front_right.transform.localRotation = Quaternion.Euler(0, h, 0);*/
     }
 
     private void followWheelRotation() {
@@ -86,6 +137,14 @@ public class CarBehaviour : MonoBehaviour {
         wc_bl.GetWorldPose(out myPosition, out myRotation);
         wheel_back_left.transform.position = myPosition;
         wheel_back_left.transform.rotation = myRotation;
+    }
+
+    private void brake(float decrement) {
+        wc_bl.brakeTorque = brakeForce;
+        wc_br.brakeTorque = brakeForce;
+        wc_fl.brakeTorque = brakeForce;
+        wc_fr.brakeTorque = brakeForce;
+        decrementSpeed(1f, decrement);
     }
 
     //Other functions
