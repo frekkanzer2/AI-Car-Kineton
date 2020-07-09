ccf3037519f73fdc1e9185fc00e17e9460d9371c
frekkanzer2 <abatefrancesco98@gmail.com>
2020-07-09 16:45:25 +0200

    Partial update of menu
    Added interation with first column of the menu

--- AI Car Kineton/Assets/Scripts/MenuController.cs
+++ AI Car Kineton/Assets/Scripts/MenuController.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuController : MonoBehaviour {
 
     class DecoButton {
         private string text;
-        private Text guiText;
+        private TMP_Text guiText;
 
-        public DecoButton(string text, Text guiText) {
+        public DecoButton(string text, TMP_Text guiText) {
             this.text = text;
             this.guiText = guiText;
         }
@@ -18,24 +19,22 @@ public class MenuController : MonoBehaviour {
             return text;
         }
 
-        public Text getGUI() {
+        public TMP_Text getGUI() {
             return guiText;
         }
 
-        public string getUnderlinedText() {
-            return "<u>" + text + "</u>";
-        }
-
         public void updateGUI(bool underlined = false) {
-            if (!underlined) this.guiText.text = text;
-            else this.guiText.text = getUnderlinedText();
+            this.guiText.text = text;
+            if (underlined) guiText.fontStyle = FontStyles.Underline;
+            else guiText.fontStyle &= ~FontStyles.Underline;
         }
     }
 
     [SerializeField]
-    private Text startText, creditsText, exitText;
-
+    private TMP_Text startText, creditsText, exitText;
     private List<DecoButton> buttons = new List<DecoButton>();
+
+    private int selection_first = 1;
 
     private void settingButtons() {
         //Populating buttons
@@ -48,15 +47,38 @@ public class MenuController : MonoBehaviour {
         buttons[0].updateGUI(true);
     }
 
+    private int changeSelectionCounter(int selectionValue, int maxValue, bool increment) {
+        //selectionValue goes from 1 to max
+        if (increment) {
+            if (selectionValue == maxValue)
+                selectionValue = 1;
+            else selectionValue += 1;
+        } else {
+            if (selectionValue == 1)
+                selectionValue = maxValue;
+            else selectionValue -= 1;
+        }
+        return selectionValue;
+    }
+
     // Start is called before the first frame update
     void Start() {
         settingButtons();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+        if (Input.GetKeyUp(KeyCode.DownArrow)) {
+            buttons[selection_first - 1].updateGUI(false);
+            selection_first = changeSelectionCounter(selection_first, buttons.Count, true);
+            buttons[selection_first - 1].updateGUI(true);
+        } else if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            buttons[selection_first - 1].updateGUI(false);
+            selection_first = changeSelectionCounter(selection_first, buttons.Count, false);
+            buttons[selection_first - 1].updateGUI(true);
+        }
+
     }
 
 }
