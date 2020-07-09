40bceaf09b1d6527c1f779d3f7dc142644ca3c7c
Carmine Ferrara <49783581+CFerrara98@users.noreply.github.com>
2020-07-09 15:41:10 +0200

    GUI Update

---
+++ AI Car Kineton/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Dio");
+            if (SceneManager.GetActiveScene().name == "ParkingScene") SceneManager.LoadScene("Menu");
+            if (SceneManager.GetActiveScene().name == "CrosswalkScene") SceneManager.LoadScene("Menu");
+        }
+    }
+}
